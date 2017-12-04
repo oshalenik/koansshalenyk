eacb1303fbdb5fe8d3cd2c292554e0b66ca2b047
oshalenik <oshalenik3105@gmail.com>
2017-12-04 18:42:33 +0200

    1

---
+++ sites/myblog2/app/models/postcomment.rb
@@ -0,0 +1,6 @@
+class Postcomment < ApplicationRecord
+	belongs_to :user
+	belongs_to :post
+	validates :text, length: { in: 1..25}
+	validates :text, presence: true
+end
---
+++ sites/myblog2/app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", small: "50x50>" }, default_url: "/images/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts    
+  has_many :photos
+  has_many :postcomments   
+
+  validates :nickname, presence: true 
+  validates :nickname, length: { in: 3..12 }
+
+
+end
